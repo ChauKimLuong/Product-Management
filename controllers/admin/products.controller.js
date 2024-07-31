3d99ddd4541943a1cc1d3d6f0f53e66db69c5304
JinKun11605 <jinkun.116.05@gmail.com>
2024-07-31 16:07:14 +0700

    2.1. Lấy data sản phẩm in ra giao diện

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,7 +1,15 @@
 // [GET] /admin/products
 
-module.exports.products = (req, res) => {
+
+const Product = require("../../models/product.model")
+
+module.exports.products = async (req, res) => {
+    const products = await Product.find({
+        deleted: false,
+    })
+    console.log(products)
     res.render("admin/pages/products/index.pug", {
-        pageTitle: "Trang sản phẩm"
+        pageTitle: "Trang sản phẩm",
+        products: products
     })
 }
