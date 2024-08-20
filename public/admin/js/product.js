213c7912e60c947302e84c3d9bff31f072b9debc
JinKun11605 <jinkun.116.05@gmail.com>
2024-08-20 09:52:12 +0700

    4. Thêm sản phẩm - Preview ảnh

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -5,11 +5,12 @@ if (buttonChangeStatus.length > 0) {
     const formChangeStatus = document.querySelector("#form-change-status");
     const path = formChangeStatus.getAttribute("data-path");
 
-    buttonChangeStatus.forEach(button => {
+    buttonChangeStatus.forEach((button) => {
         button.addEventListener("click", () => {
             const currentStatus = button.getAttribute("data-status");
             const id = button.getAttribute("data-id");
-            let statusAfterChange = (currentStatus === "active" ? "inactive" : "active");
+            let statusAfterChange =
+                currentStatus === "active" ? "inactive" : "active";
 
             const action = `${path}/${statusAfterChange}/${id}?_method=PATCH`;
             console.log(`Form action: ${action}`); // Kiểm tra URL và phương thức gửi đi
@@ -22,24 +23,44 @@ if (buttonChangeStatus.length > 0) {
 // End Change status
 
 // Delete Item
-const buttonsDelete = document.querySelectorAll("[button-delete]")
+const buttonsDelete = document.querySelectorAll("[button-delete]");
 
-if (buttonsDelete.length > 0){
-    const formDeleteItem = document.querySelector("#form-delete-item")
-    let path = formDeleteItem.getAttribute("data-path")
+if (buttonsDelete.length > 0) {
+    const formDeleteItem = document.querySelector("#form-delete-item");
+    let path = formDeleteItem.getAttribute("data-path");
 
-    buttonsDelete.forEach(button => {
+    buttonsDelete.forEach((button) => {
         button.addEventListener("click", () => {
-            isConfirm = confirm("Xóa ?")
-            if (isConfirm){
-                const id = button.getAttribute("data-id")
-                const action = `${path}/${id}?_method=DELETE`
+            isConfirm = confirm("Xóa ?");
+            if (isConfirm) {
+                const id = button.getAttribute("data-id");
+                const action = `${path}/${id}?_method=DELETE`;
 
-                formDeleteItem.action = action
-                formDeleteItem.submit()
+                formDeleteItem.action = action;
+                formDeleteItem.submit();
             }
-        })
-    })
+        });
+    });
 }
-
 // End Delete Item
+
+// Preview Image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+    const uploadImageInput = document.querySelector("[upload-image-input]");
+    const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+    uploadImageInput.addEventListener("change", (event) => {
+        const file = event.target.files[0];
+        uploadImagePreview.src = URL.createObjectURL(file);
+
+        const buttonDeleteImage = document.querySelector("[button-delete-image]");
+        if (buttonDeleteImage) {
+            buttonDeleteImage.addEventListener("click", () => {
+                uploadImageInput.value = "";
+                uploadImagePreview.src = "";
+            });
+        }
+    });
+}
+// End Preview Image
