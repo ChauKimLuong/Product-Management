57d1c965784e2c78e0f1f11801dca3c420366973
JinKun11605 <jinkun.116.05@gmail.com>
2024-08-23 12:16:58 +0700

    5.1. MongoDB Atlas

--- config/database.js
+++ config/database.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose")
 
 module.exports.connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log("Connect Success!")
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connect Success!");
     } catch (error) {
-        console.log("Connect Error!")
+        console.error("Connect Error:", error.message);
     }
+    
 }
 
 
