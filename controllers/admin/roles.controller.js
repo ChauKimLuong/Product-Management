ca4507092df7bbdba1d24f03d4ff5d42733580ef
JinKun11605 <jinkun.116.05@gmail.com>
2024-09-17 00:21:37 +0700

    3.4. Chỉnh sửa phần Nhóm quyền

--- controllers/admin/roles.controller.js
+++ controllers/admin/roles.controller.js
@@ -14,17 +14,46 @@ module.exports.index = async (req, res) => {
     })
 }
 
-// [GET] /admin/create
+// [GET] /admin/roles/create
 module.exports.create = async (req, res) => {
     res.render("admin/pages/roles/create", {
         pageTitle: "Nhóm quyền",
     })
 }
 
-// [POST] /admin/createPost
+// [POST] /admin/roles/create
 module.exports.createPost = async (req, res) => {
     const record = new Role(req.body);
     await record.save();
 
     res.redirect(`${systemConfig.prefixAdmin}/roles`)
+}
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id
+    
+        const record = await Role.findOne({ _id: id })
+
+        res.render("admin/pages/roles/edit", {
+            pageTitle: "Chỉnh sửa nhóm quyền",
+            record: record,
+        })
+    } catch (error) {
+        req.flash("error", "Không tìm thấy nhóm quyền!");
+        res.redirect(`${systemConfig.prefixAdmin}/roles`);
+    }
+}
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+    try {
+        await Role.updateOne({_id: req.params.id}, req.body)
+
+        req.flash("success", "Cập nhật thành công!")
+    } catch (error) {
+        req.flash("error", "Cập nhật thất bại!");
+    }
+    res.redirect("back")
 }
