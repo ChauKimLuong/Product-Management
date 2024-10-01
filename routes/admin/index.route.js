f37370cc57a3aa7b265b9864f66781943eb1d5f6
JinKun11605 <jinkun.116.05@gmail.com>
2024-10-02 00:20:55 +0700

    4. Thêm middleware cho Route private.

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,19 +1,43 @@
-const systemConfig = require("../../config/system")
+const systemConfig = require("../../config/system");
 
-const dashboardRoute = require("./dashboard.route.js")
-const productRoute = require("./products.route.js") 
-const productCategoryRoute = require("./product-category.route.js")
-const roleRoute = require("./roles.route.js")
-const accountRoute = require("./accounts.route.js")
-const authRoute = require("./auth.route.js")
+const dashboardRoute = require("./dashboard.route.js");
+const productRoute = require("./products.route.js");
+const productCategoryRoute = require("./product-category.route.js");
+const roleRoute = require("./roles.route.js");
+const accountRoute = require("./accounts.route.js");
+
+const authRoute = require("./auth.route.js");
+const authMiddleware = require("../../middlewares/admin/auth.middleware.js");
 
 module.exports = (app) => {
-    const PATH_ADMIN = systemConfig.prefixAdmin
+    const PATH_ADMIN = systemConfig.prefixAdmin;
 
-    app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute)
-    app.use(`${PATH_ADMIN}/products`, productRoute)
-    app.use(`${PATH_ADMIN}/product-category`, productCategoryRoute)
-    app.use(`${PATH_ADMIN}/roles`, roleRoute)
-    app.use(`${PATH_ADMIN}/accounts`, accountRoute)
-    app.use(`${PATH_ADMIN}/auth`, authRoute)
-}
+    app.use(
+        `${PATH_ADMIN}/dashboard`,
+        authMiddleware.requireAuth,
+        dashboardRoute
+    );
+
+    app.use(`${PATH_ADMIN}/products`, 
+        authMiddleware.requireAuth, 
+        productRoute
+    );
+
+    app.use(
+        `${PATH_ADMIN}/product-category`,
+        authMiddleware.requireAuth,
+        productCategoryRoute
+    );
+
+    app.use(`${PATH_ADMIN}/roles`, 
+        authMiddleware.requireAuth, 
+        roleRoute
+    );
+
+    app.use(`${PATH_ADMIN}/accounts`, 
+        authMiddleware.requireAuth, 
+        accountRoute
+    );
+
+    app.use(`${PATH_ADMIN}/auth`, authRoute);
+};
