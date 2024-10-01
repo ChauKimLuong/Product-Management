b6b0f060d9996dfdce64bb71cdc4a42a0dd05c4f
JinKun11605 <jinkun.116.05@gmail.com>
2024-10-01 11:07:27 +0700

    3. Tính năng đăng xuất.

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -41,3 +41,11 @@ module.exports.loginPost = async (req, res) => {
     res.cookie("token", user.token)
     res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
 }
+
+
+// [GET] /admin/auth/logout
+module.exports.logout = (req, res) => {
+    res.clearCookie("token")
+    console.log(systemConfig.prefixAdmin);
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+}
