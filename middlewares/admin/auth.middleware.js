5562987b9016b910135d80d384bfaf4f7453da4d
JinKun11605 <jinkun.116.05@gmail.com>
2024-10-04 23:33:24 +0700

    22.1. Thêm Authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -2,14 +2,20 @@ const systemConfig = require("../../config/system");
 const Account = require("../../models/accounts.model")
 
 module.exports.requireAuth = async (req, res, next) => {
-    if (!req.cookies.token){
+    if (!req.cookies.token) {
         res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     } else {
-        const user = await Account.findOne({ token: req.cookies.token})
-        
-        if (!user){
+        const user = await Account.findOne({ token: req.cookies.token })
+            .select("-password")
+            .populate({ path: "role_id", select: "title permissions" }); // Chỉ lấy ra title và permissions
+
+        if (!user) {
             res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
         } else {
+            res.locals.user = user;
+            res.locals.role = user.role_id;
+            // console.log(user)
+            // console.log(user.role_id);
             next();
         }
     }
