857921aa4684c95dcc9cda7fd5239a3c92d6eebf
JinKun11605 <jinkun.116.05@gmail.com>
2024-10-16 23:48:38 +0700

    4.2. Danh sách sản phẩm mới nhất.

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -3,17 +3,31 @@ const productHelper = require("../../helpers/product.js")
 
 // [GET] /
 module.exports.index = async (req, res) => {
-    // Sẩn phầm nổi bật
+// Sản phầm nổi bật
     const featuredProducts = await Product.find({
         featured: "1", 
         deleted: false,
         status: "active",
     }).limit(3)
 
-    const newProducts = productHelper.newProductPrice(featuredProducts)
+    const newFeaturedProducts = productHelper.newProductPrice(featuredProducts)
+// Sản phầm nổi bật
+
+// Sản phầm nới nhất
+    const newestProducts = await Product
+    .find({
+        deleted: false,
+        status: "active",
+    })
+    .sort({ position: -1 })
+    .limit(4)
+
+    const newestProductsPrice = productHelper.newProductPrice(newestProducts)
+// Sản phầm nới nhất
 
     res.render("client/pages/home/index", {
         pageTitle: "Trang ông chủ",
-        featuredProducts: newProducts,
+        newFeaturedProducts: newFeaturedProducts,
+        newestProducts: newestProductsPrice,
     })
 }
