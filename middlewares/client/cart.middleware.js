6e594d120638161241bc64b1fc7a21035aa2aa3b
JinKun11605 <jinkun.116.05@gmail.com>
2024-10-24 23:32:02 +0700

    2.2. Làm nút MiniCart trên Header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -12,7 +12,11 @@ module.exports.cartId = async (req, res, next) => {
             httpOnly: true
         });
     } else {
-        
+        const cart = await Cart.findOne({ _id: req.cookies.cartId });
+
+        cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0)
+
+        res.locals.miniCart = cart;
     }
     next();
 };
