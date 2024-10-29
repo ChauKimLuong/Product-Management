e73b22fef183265b42643789d23a890d7abe20e4
JinKun11605 <jinkun.116.05@gmail.com>
2024-10-30 02:45:24 +0700

    2.4. Xóa sản phẩm trong Giỏ hàng.

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,57 +1,72 @@
 const Cart = require("../../models/cart.model.js");
 const Product = require("../../models/product.model.js");
-const productHelper = require("../../helpers/product.js")
+const productHelper = require("../../helpers/product.js");
 
 // [GET] /cart
 module.exports.index = async (req, res) => {
-  const cart = await Cart.findOne({ _id: req.cookies.cartId });
-  let totalPriceAll = 0;
+    const cart = await Cart.findOne({ _id: req.cookies.cartId });
+    let totalPriceAll = 0;
 
-  for (const product of cart.products) {
-    let productInfo = await Product.findOne({
-      _id: product.product_id,
-      deleted: false, 
-      status: "active"
-    }).select("title thumbnail price discountPercentage slug");
+    for (const product of cart.products) {
+        let productInfo = await Product.findOne({
+            _id: product.product_id,
+            deleted: false,
+            status: "active",
+        }).select("title thumbnail price discountPercentage slug");
 
-    productInfo = productInfo.toObject(); // ! Vì productInfo là tài lại Mongoose mặc định định là immutable
-    productInfo = productHelper.newOneProductPrice(productInfo);
+        productInfo = productInfo.toObject(); // ! Vì productInfo là tài lại Mongoose mặc định định là immutable
+        productInfo = productHelper.newOneProductPrice(productInfo);
 
-    totalPrice = productInfo.newPrice * product.quantity;
-    totalPriceAll += totalPrice;
+        totalPrice = productInfo.newPrice * product.quantity;
+        totalPriceAll += totalPrice;
 
-    product.productInfo = productInfo;
-    product.totalPrice = totalPrice;
-  }
-  cart.totalPriceAll = totalPriceAll;
+        product.productInfo = productInfo;
+        product.totalPrice = totalPrice;
+    }
+    cart.totalPriceAll = totalPriceAll;
 
-  res.render("client/pages/cart/index", {
-    pageTitle: "Giỏ hàng",
-    cart: cart,
-  });
+    res.render("client/pages/cart/index", {
+        pageTitle: "Giỏ hàng",
+        cart: cart,
+    });
 };
+
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
-  const productId = req.params.productId;
-  const quantity = parseInt(req.body.quantity);
-  const cartId = req.cookies.cartId;
+    const productId = req.params.productId;
+    const quantity = parseInt(req.body.quantity);
+    const cartId = req.cookies.cartId;
 
-  const cart = await Cart.findOne({ "products.product_id": productId });
+    const product = await Cart.findOne({ "products.product_id": productId });
 
-  if (cart) {
+    if (product) {
+        await Cart.updateOne(
+            { _id: cartId, "products.product_id": productId },
+            { $inc: { "products.$.quantity": quantity } }
+        );
+    } else {
+        const objCart = {
+            product_id: productId,
+            quantity: quantity,
+        };
+
+        await Cart.updateOne({ _id: cartId }, { $push: { products: objCart } });
+    }
+
+    req.flash("success", "Thêm sản phẩm thành công!");
+    res.redirect("/");
+};
+
+// [GET] /cart/delete/:productId
+module.exports.delete = async (req, res) => {
+    const productId = req.params.productId;
+
     await Cart.updateOne(
-      { _id: cartId, "products.product_id": productId },
-      { $inc: { "products.$.quantity": quantity } }
+        { _id: req.cookies.cartId }, 
+        { $pull: { products: { product_id: productId }}},
     );
-  } else {
-    const objCart = {
-      product_id: productId,
-      quantity: quantity,
-    };
 
-    await Cart.updateOne({ _id: cartId }, { $push: { products: objCart } });
-  }
 
-  req.flash("success", "Thêm sản phẩm thành công!");
-  res.redirect("/");
+    req.flash("success", "Xóa sản phẩm thành công!");
+    res.redirect("back");
 };
