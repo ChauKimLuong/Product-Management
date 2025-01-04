4ccd1d14946706f60345ba8d27ad429bd8248026
JinKun11605 <jinkun.116.05@gmail.com>
2025-01-05 00:50:45 +0700

    2.3. Đăng nhập Client

--- validate/client/user.validate.js
+++ validate/client/user.validate.js
@@ -23,3 +23,20 @@ module.exports.registerPost = (req, res, next) => {
     
     next();
 }
+
+
+module.exports.loginPost = (req, res, next) => {
+    if (!req.body.email) {
+        req.flash("error", "Vui lòng nhập Email!");
+        res.redirect("back");
+        return;
+    }
+
+    if (!req.body.password) {
+        req.flash("error", "Vui lòng nhập Mật khẩu!");
+        res.redirect("back");
+        return;
+    }
+    
+    next();
+}
