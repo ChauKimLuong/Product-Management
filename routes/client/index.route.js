242a75af08838d43843e61fc892f4cf19a4a6d43
JinKun11605 <jinkun.116.05@gmail.com>
2025-01-19 04:19:00 +0700

    28.1.2. Học về Socket.io / 28.3. Làm giao diện Chat

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,33 +1,29 @@
-const productRoutes = require("./product.route")
-const homeRoutes = require("./home.route")
-const searchRoutes = require("./search.routes")
-const cartRoutes = require("./cart.routes")
-const checkoutRoutes = require("./checkout.routes")
-const userRoutes = require("./user.routes")
-const passwordRoutes = require("./password.routes")
+const productRoutes = require("./product.route");
+const homeRoutes = require("./home.route");
+const searchRoutes = require("./search.routes");
+const cartRoutes = require("./cart.routes");
+const checkoutRoutes = require("./checkout.routes");
+const userRoutes = require("./user.routes");
+const passwordRoutes = require("./password.routes");
+const chatRoutes = require("./chat.routes");
 
-const categoryMiddleware = require("../../middlewares/client/category.middleware")
-const cartMiddleware = require("../../middlewares/client/cart.middleware")
-const userMiddleware = require("../../middlewares/client/user.middleware")
-const settingMiddleware = require("../../middlewares/client/setting.middleware")
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const cartMiddleware = require("../../middlewares/client/cart.middleware");
+const userMiddleware = require("../../middlewares/client/user.middleware");
+const settingMiddleware = require("../../middlewares/client/setting.middleware");
 
 module.exports = (app) => {
-    app.use(categoryMiddleware.category)
-    app.use(cartMiddleware.cartId)
-    app.use(userMiddleware.infoUser)
-    app.use(settingMiddleware.settingGeneral)
+    app.use(categoryMiddleware.category);
+    app.use(cartMiddleware.cartId);
+    app.use(userMiddleware.infoUser);
+    app.use(settingMiddleware.settingGeneral);
 
-    app.use("/", homeRoutes)
-    
-    app.use("/products", productRoutes)
-
-    app.use("/search", searchRoutes)
-
-    app.use("/cart", cartRoutes)
-
-    app.use("/checkout", checkoutRoutes)
-
-    app.use("/user", userRoutes)
-
+    app.use("/", homeRoutes);
+    app.use("/products", productRoutes);
+    app.use("/search", searchRoutes);
+    app.use("/cart", cartRoutes);
+    app.use("/checkout", checkoutRoutes);
+    app.use("/user", userRoutes);
     app.use("/password", passwordRoutes);
-} 
+    app.use("/chat", chatRoutes);
+};
