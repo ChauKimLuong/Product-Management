3e61135dcf46ba129241da8f90b1c8cf0abea29b
JinKun11605 <jinkun.116.05@gmail.com>
2025-02-18 02:04:07 +0700

    Tách thành folder Socket riêng

--- sockets/client/chat.socket.js
+++ sockets/client/chat.socket.js
@@ -1,34 +1,31 @@
 const Chat = require("../../models/chat.model");
-const User = require("../../models/user.model");
 
 const uploadToCloudinary = require("../../helpers/uploadToCloudinary")
-const chatSocket = require("../../sockets/client/chat.socket")
 
-// [GET] /chat
-module.exports.index = async (req, res) => {
+
+module.exports = (res) => {
     const userID = res.locals.user.id;
     const fullName = res.locals.user.fullName;
 
-    // CẤU HÌNH Socket.io
     _io.once("connection", (socket) => {
         console.log("ID nguời dùng kết nối:", socket.id);
-
+    
         socket.on("CLIENT_SEND_MESSAGE", async (data) => {
             let images = [];
-
+    
             for (const imageBuffer of data.images) {
                 const link = await uploadToCloudinary(imageBuffer);
                 images.push(link);
             }
-
+    
             const chat = new Chat({
                 user_id: userID,
                 content: data.content,
                 images: images
             })
-
+    
             await chat.save();
-
+    
             //? Trả message mới nhất về CLIENT
             _io.emit("SERVER_RETURN_MESSAGE", {
                 userID: userID,
@@ -37,7 +34,7 @@ module.exports.index = async (req, res) => {
                 images: images
             });
         })
-
+    
         socket.on("CLIENT_SEND_TYPING", async (status) => {
             //? Trả type về CLIENT
             _io.emit("SERVER_RETURN_TYPING", {
@@ -46,23 +43,5 @@ module.exports.index = async (req, res) => {
                 isTyping: status,
             });
         })
-    })
-    // END CẤU HÌNH Socket.io
-
-    // LẤY DATA TỪ DB
-    const messages = await Chat.find({ deleted: false });
-
-    for (const message of messages) {
-        const infoUser = await User.findOne({ _id: message.user_id }).select("fullName avatar");
-
-        message.infoUser = infoUser;
-    }
-    // END LẤY DATA TỪ DB
-
-
-
-    res.render("client/pages/chat/index", {
-        pageTitle: "Chat",
-        messages: messages,
     })
 }
