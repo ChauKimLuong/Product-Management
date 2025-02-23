4254befb2ed4a45ece30dcd798bc56e0731e1162
JinKun11605 <jinkun.116.05@gmail.com>
2025-02-24 03:33:28 +0700

    30.1.3. Gửi yêu cầu

--- public/js/friend.js
+++ public/js/friend.js
@@ -1,1 +1,17 @@
-console.log("hehhe")
+//* YÊU CẦU KẾT BẠN
+const btnAddFriend = document.querySelectorAll("[btn-add-friend]");
+
+if (btnAddFriend.length > 0){
+    btnAddFriend.forEach(btn => {
+        btn.addEventListener("click", () => {
+            const clickedUserId = btn.getAttribute("btn-add-friend");
+
+            const boxUser = btn.closest(".box-user");
+            boxUser.classList.toggle("add");
+
+
+            socket.emit("CLIEND_ADD_FRIEND", clickedUserId);
+        })
+    });
+}
+//! END YÊU CẦU KẾT BẠN
